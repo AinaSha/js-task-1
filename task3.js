4d7c9405d127ed86e808b32b853cd074c4f37cfc
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2022-01-27 03:58:00 +0600

    fix: divide tasks into files

---
+++ task3.js
@@ -0,0 +1,7 @@
+// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let data = "Ainagul Shabdanova";
+let mail = "shabdanova.ainagul.kk@gmail.com";
+
+alert(`My name: ${data}`);
+alert(`E-mail: ${mail}`);
